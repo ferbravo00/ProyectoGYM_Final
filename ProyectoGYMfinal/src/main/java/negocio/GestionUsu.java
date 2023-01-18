b117bed454b5b0be6fdf52c0aa12ea781bd1fa89
ferbravo00 <fer.bp001@gmail.com>
2023-01-18 13:01:41 +0100

    cambios

--- ProyectoGYMfinal/src/main/java/negocio/GestionUsu.java
+++ ProyectoGYMfinal/src/main/java/negocio/GestionUsu.java
@@ -8,24 +8,53 @@ import datos.*;
 import dominio.Usuario;
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
+import java.util.List;
+import javax.inject.Inject;
+
 /**
  *
  * @author Alumno Mañana
  */
-public class GestionUsu{
-    private InterfazUsuario datos;
+public class GestionUsu implements InterfazGestionUsu{
+    @Inject    
+    private InterfazUsuario usuarioDao;
 
-    public GestionUsu() {
-        this.datos = new UsuarioDao();
+    @Override
+    public List<Usuario> listarUsuarios() {       
+        return usuarioDao.findAllUsuarios();
+    }
+
+    @Override
+    public Usuario encontrarUsuarioPorID(Usuario usuario) {
+        return usuarioDao.findUsuarioByID(usuario);
+    }
+
+    @Override
+    public Usuario econtrarUsuarioPorNombre(Usuario usuario) {
+        return usuarioDao.findUsuarioByNombre(usuario);
+    }
+
+    @Override
+    public void registrarUsuario(Usuario usuario) {
+        usuarioDao.insertUsuario(usuario);
+    }
+
+    @Override
+    public void modificarUsuario(Usuario usuario) {
+        usuarioDao.updateUsuario(usuario);
+    }
+
+    @Override
+    public void eliminarUsuario(Usuario usuario) {
+        usuarioDao.deleteUsuario(usuario);
     }
     
+    @Override
     public boolean comprobar(Usuario usu){
-        int num = this.datos.findAllUsuarios().size();     //Lo he tenido que meter en una variable para que funcione...
+        int num = this.usuarioDao.findAllUsuarios().size();     //Lo he tenido que meter en una variable para que funcione...
         for (int i = 0; i < num; i++) {
             //System.out.println(seleccionar().size());
-            if(this.datos.findAllUsuarios().get(i).getNombre().equalsIgnoreCase(usu.getNombre()) && this.datos.findAllUsuarios().get(i).getClave().equalsIgnoreCase(usu.getClave())){
+            if(this.usuarioDao.findAllUsuarios().get(i).getNombre().equalsIgnoreCase(usu.getNombre()) && this.usuarioDao.findAllUsuarios().get(i).getClave().equalsIgnoreCase(usu.getClave())){
                 return true;
             }
         }
@@ -33,7 +62,7 @@ public class GestionUsu{
     }
       
     
- 
+    @Override
     public String cifrarMD5(String input) throws Exception {
         String md5 = null;
         if (null == input)
---
+++ ProyectoGYMfinal/src/main/java/negocio/InterfazGestionUsu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+import dominio.Usuario;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+@Local
+public interface InterfazGestionUsu {
+    public List<Usuario> listarUsuarios();
+    
+    public Usuario encontrarUsuarioPorID(Usuario usuario);
+    
+    public Usuario econtrarUsuarioPorNombre(Usuario usuario);
+    
+    public void registrarUsuario(Usuario usuario);
+    
+    public void modificarUsuario(Usuario usuario);
+    
+    public void eliminarUsuario(Usuario usuario);
+    
+    public boolean comprobar(Usuario usu);
+    
+    public String cifrarMD5(String input) throws Exception;
+}
